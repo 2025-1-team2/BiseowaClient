7f380dd6bfd7a88f6a2dbd6c40b761d58c56397c
sujin01 <r5870@naver.com>
2025-06-03 21:26:22 +0900

    Initial Commit

---
+++ BiseowaClient/BiseowaClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiseowaClientApp.swift
+//  BiseowaClient
+//
+//  Created by 김수진 on 6/3/25.
+//
+
+import SwiftUI
+
+@main
+struct BiseowaClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BiseowaClientTests/BiseowaClientTests.swift
@@ -0,0 +1,17 @@
+//
+//  BiseowaClientTests.swift
+//  BiseowaClientTests
+//
+//  Created by 김수진 on 6/3/25.
+//
+
+import Testing
+@testable import BiseowaClient
+
+struct BiseowaClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
