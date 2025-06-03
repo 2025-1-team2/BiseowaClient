e509cd30c3c6f6c69b9e3001d35bb2445588c87e
JungSuIn <suin6327@naver.com>
2025-06-04 02:39:04 +0900

    [App-28] feat ParticipantView(개인화면)구조체 초기 구현

--- BiseowaClient/VideoConference/View/ParticipantView.swift
+++ BiseowaClient/VideoConference/View/ParticipantView.swift
@@ -8,11 +8,34 @@
 import SwiftUI
 
 struct ParticipantView: View {
+    let name: String
+    let isLocalUser: Bool
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack(spacing: 10) {
+            ZStack {
+                // 더 큰 네모 박스
+                RoundedRectangle(cornerRadius: 12)
+                    .fill(Color.white)
+                    .frame(width: 150, height: 150) // 네모 크기 키움
+
+                // 사람 아이콘을 작게 (여백 생기도록)
+                Image(systemName: "person.fill")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 80, height: 80) // 👈 작게 줄임
+                    .foregroundColor(isLocalUser ? Color("BackgroundMint") : .black)
+            }
+            Text(name)
+                .font(.caption)
+                .foregroundColor(.black)
+                .padding(.horizontal, 12)
+                .padding(.vertical, 4)
+                .background(Color(.systemGray5))
+                .cornerRadius(10)
+        }
+        .padding(6)
     }
 }
 
-#Preview {
-    ParticipantView()
-}
+
