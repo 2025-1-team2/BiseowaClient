c55ac64f66276e2c12824e211236b10efe8932de
sujin01 <r5870@naver.com>
2025-06-04 01:49:58 +0900

    [APP-29] add files

---
+++ BiseowaClient/Auth/Service/GoogleSignInManager.swift
@@ -0,0 +1,7 @@
+//
+//  GoogleSignInManager.swift
+//  BiseowaClient
+//
+//  Created by 김수진 on 6/4/25.
+//
+
---
+++ BiseowaClient/Auth/ViewModel/AuthViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  AuthViewModel.swift
+//  BiseowaClient
+//
+//  Created by 김수진 on 6/4/25.
+//
+
